bebe8062177b24a7a83e0c6a5281b1ed613a16b2
Oleg Berezin <olegberezin2002@gmail.com>
2025-09-22 15:10:34 +0300

    [master] Refactoring

--- problems/2026/02_syst_array/src/sa/matrix_mul.cpp
+++ problems/2026/02_syst_array/src/sa/matrix_mul.cpp
@@ -1,27 +1,22 @@
-// matrix_mul.cpp
 #include <cstdint>
 
-// Параметры должны совпадать с теми, что в TB/DUT
-static constexpr int SIZE    = 4;
-static constexpr int I_WIDTH = 16;
-static constexpr int O_WIDTH = I_WIDTH * SIZE - SIZE;
+#define SIZE 4
 
-// Эквивалент SystemVerilog typedef struct packed { logic [O_WIDTH-1:0] matrix[SIZE][SIZE]; }
 typedef struct {
-    uint64_t matrix[SIZE][SIZE];
+    uint64_t data[SIZE][SIZE];
 } matrix_t;
 
 extern "C" matrix_t matrix_mul(const matrix_t a, const matrix_t b) {
-    matrix_t c{}; // zero-init
+    matrix_t c{};
 
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
             uint64_t sum = 0;
             for (int k = 0; k < SIZE; ++k) {
-                uint64_t mul = a.matrix[i][k] * b.matrix[k][j]; // без контроля переполнений
-                sum += mul;                                      // без масок/обрезки
+                uint64_t mul = a.data[i][k] * b.data[k][j];
+                sum += mul;
             }
-            c.matrix[i][j] = sum;
+            c.data[i][j] = sum;
         }
     }
     return c;
