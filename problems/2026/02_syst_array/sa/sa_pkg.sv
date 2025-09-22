9b2e2cda624b4d0989c158473722a90f4a33806d
Oleg Berezin <olegberezin2002@gmail.com>
2025-09-22 13:24:05 +0300

    [master] Create tb

--- problems/2026/02_syst_array/sa/sa_pkg.sv
+++ problems/2026/02_syst_array/sa/sa_pkg.sv
@@ -1,3 +1,12 @@
 package sa_pkg;
 
+localparam SIZE = 4;
+localparam I_WIDTH = 16;
+localparam MATRIX_WIDTH = I_WIDTH * SIZE - SIZE;
+
+
+typedef struct packed {
+    logic [MATRIX_WIDTH-1:0] matrix [SIZE][SIZE];
+} matrix_t;
+
 endpackage
