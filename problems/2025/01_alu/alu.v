962d3f763df423c521f3e499bc7bd00ae07e0800
Oleg Berezin <olegberezin2002@gmail.com>
2026-03-09 19:25:59 +0300

    Add R-type ALU

---
+++ problems/2025/01_alu/alu.v
@@ -0,0 +1,39 @@
+typedef enum reg [3:0] {
+    ADD,
+    SUB,
+    SLL,
+    SLT,
+    SLTU,
+    XOR,
+    SRL,
+    SRA,
+    OR,
+    AND
+} op_t;
+
+module alu (
+    input  wire [31:0] i_a,
+    input  wire [31:0] i_b,
+
+    input  op_t i_op,
+
+    output reg [31:0] o_res
+);
+
+always @(*) begin
+    case (i_op)
+        ADD:     o_res = i_a + i_b;
+        SUB:     o_res = i_a - i_b;
+        SLL:     o_res = i_a << i_b[4:0];
+        SLT:     o_res = $signed(i_a) < $signed(i_b);
+        SLTU:    o_res = i_a < i_b;
+        XOR:     o_res = i_a ^ i_b;
+        SRL:     o_res = i_a >> i_b[4:0];
+        SRA:     o_res = $signed(i_a) >>> i_b[4:0];
+        OR:      o_res = i_a | i_b;
+        AND:     o_res = i_a & i_b;
+        default: o_res = 'X;
+    endcase
+end
+
+endmodule
