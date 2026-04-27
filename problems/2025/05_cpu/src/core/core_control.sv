944f75bcfca999c759f7895afcdce47cc16ea8ba
Oleg Berezin <olegberezin2002@gmail.com>
2026-04-27 12:05:23 +0300

    Add alu op for OP

--- problems/2025/05_cpu/src/core/core_control.sv
+++ problems/2025/05_cpu/src/core/core_control.sv
@@ -17,12 +17,23 @@ always_comb begin
                 3'b110:  o_alu_op = OR;      // ORI
                 3'b111:  o_alu_op = AND;     // ANDI
                 3'b001:  o_alu_op = SLL;     // SLLI
-                3'b101:  o_alu_op = i_isntr.payload.i.imm_u.shift_op.funct7[5] ? SRA    // SRAI
-                                                                               : SRL;   // SRLI
+                3'b101:  o_alu_op = !i_isntr.payload.i.imm_u.shift_op.funct7[5] ? SRL    // SRLI
+                                                                                : SRA;   // SRAI
             endcase
         end
         OP: begin
-            
+            case (i_isntr.payload.r.funct3)
+                3'b000: o_alu_op = !i_isntr.payload.r.funct7[5] ? ADD  // ADD
+                                                                : SUB; // SUB
+                3'b001: o_alu_op = SLL; // SLL
+                3'b010: o_alu_op = SLT; // SLT
+                3'b011: o_alu_op = SLTU; // SLTU
+                3'b100: o_alu_op = XOR; // XOR
+                3'b101: o_alu_op = !i_isntr.payload.r.funct7[5] ? SRL // SRL
+                                                                : SRA; // SRA
+                3'b110: o_alu_op = OR; // OR
+                3'b111: o_alu_op = AND; // AND
+            endcase
         end
     endcase
 end
