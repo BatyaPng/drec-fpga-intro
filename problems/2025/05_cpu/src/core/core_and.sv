33204e97ce094ec1a2ee1f2f645a25922901f914
Oleg Berezin <olegberezin2002@gmail.com>
2026-05-01 15:28:50 +0300

    Add common unit

---
+++ problems/2025/05_cpu/src/core/core_and.sv
@@ -0,0 +1,10 @@
+module core_and (
+    input  logic i_a,
+    input  logic i_b,
+
+    output logic i_c
+);
+
+assign i_c = i_a & i_b;
+
+endmodule
---
+++ problems/2025/05_cpu/src/core/core_mux4.sv
@@ -0,0 +1,11 @@
+module core_mux4 (
+    input logic [1:0] i_sel,
+
+    input logic [1:0][31:0] i_data,
+
+    output logic [31:0] o_data
+);
+
+assign o_data = i_data[i_sel];
+
+endmodule
