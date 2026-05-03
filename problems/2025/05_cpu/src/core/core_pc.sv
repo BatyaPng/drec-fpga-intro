3a30f98d6d0f66865452aa2a05c28d4ce98ac92f
Oleg Berezin <olegberezin2002@gmail.com>
2026-05-03 21:03:24 +0300

    Use 32 bit width in pc

--- problems/2025/05_cpu/src/core/core_pc.sv
+++ problems/2025/05_cpu/src/core/core_pc.sv
@@ -3,20 +3,24 @@ module core_pc (
     input  logic rst_n,
 
     input  logic        i_branch,
-    input  logic [29:0] i_branch_addr,
+    input  logic [31:0] i_branch_addr,
 
-    output logic [29:0] o_instr_addr
+    output logic [31:0] o_pc,
+    output logic [31:0] o_pc_next
 );
 
-logic [29:0] pc_next;
-logic [29:0] pc_ff;
+logic [31:0] pc_next;
+logic [31:0] pc_ff;
 
-assign pc_next = i_branch ? i_branch_addr : pc_ff + 1;
+assign pc_next = i_branch ? i_branch_addr : pc_ff + 4;
 
 always_ff @(posedge clk or negedge rst_n)
     if (!rst_n)
-        pc_ff <= 30'h1000;
+        pc_ff <= 32'h1000;
     else
         pc_ff <= pc_next;
+
+assign o_pc      = pc_ff;
+assign o_pc_next = pc_next;
 
 endmodule
