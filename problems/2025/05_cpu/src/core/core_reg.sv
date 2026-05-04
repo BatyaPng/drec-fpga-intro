aa331ca9b70e17e18dfa5078f64e5f497ba261d2
Oleg Berezin <olegberezin2002@gmail.com>
2026-05-04 12:15:29 +0300

    Fix width

--- problems/2025/05_cpu/src/core/core_reg.sv
+++ problems/2025/05_cpu/src/core/core_reg.sv
@@ -5,8 +5,8 @@ module core_reg (
     input  logic [4:0] i_rs1,
     input  logic [4:0] i_rs2,
 
-    input  logic [4:0]  i_dst,
-    input  logic [31:0] i_rd,
+    input  logic [31:0] i_dst,
+    input  logic  [4:0] i_rd,
 
     output logic [31:0] o_src1,
     output logic [31:0] o_src2
@@ -21,7 +21,7 @@ always_ff @(posedge clk or negedge rst_n)
     if (!rst_n)
         reg_ff <= '0;
     else if (reg_en)
-        reg_ff[i_dst] <= i_rd;
+        reg_ff[i_rd] <= i_dst;
 
 assign o_src1 = reg_ff[i_rs1];
 assign o_src2 = reg_ff[i_rs2];
