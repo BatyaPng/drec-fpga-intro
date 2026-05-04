2fe15a8ca326eb81303fd89506965b777017d069
Oleg Berezin <olegberezin2002@gmail.com>
2026-05-04 13:14:01 +0300

    Refactoring

--- problems/2025/05_cpu/src/core/core_lsu.sv
+++ problems/2025/05_cpu/src/core/core_lsu.sv
@@ -6,48 +6,48 @@ module core_lsu
     input  logic [31:0] i_data_core2mem,
     input  logic [31:0] i_data_mem2core,
 
-    output logic [29:0] o_data_core2mem_addr,
-    output logic [31:0] o_data_core2mem_data,
-    output logic        o_data_core2mem_we,
-    output logic [3:0]  o_data_core2mem_mask,
+    output logic [29:0] o_core2mem_addr,
+    output logic [31:0] o_core2mem_data,
+    output logic        o_core2mem_we,
+    output logic [3:0]  o_core2mem_mask,
 
-    output logic [31:0] o_data_mem2core_data
+    output logic [31:0] o_mem2core_data
 );
 
-    assign o_data_core2mem_addr = i_addr;
+    assign o_core2mem_addr = i_addr;
 
     always_comb begin
-        o_data_core2mem_we   = 1'b0;
-        o_data_core2mem_mask = 4'b0000;
-        o_data_core2mem_data = i_data_core2mem;
+        o_core2mem_we   = 1'b0;
+        o_core2mem_mask = 4'b0000;
+        o_core2mem_data = i_data_core2mem;
 
         case (i_mem_op)
             SB: begin
-                o_data_core2mem_we   = 1'b1;
-                o_data_core2mem_mask = 4'b0001;
-                o_data_core2mem_data = {4{i_data_core2mem[7:0]}};
+                o_core2mem_we   = 1'b1;
+                o_core2mem_mask = 4'b0001;
+                o_core2mem_data = {4{i_data_core2mem[7:0]}};
             end
             SH: begin
-                o_data_core2mem_we   = 1'b1;
-                o_data_core2mem_mask = 4'b0011;
-                o_data_core2mem_data = {2{i_data_core2mem[15:0]}};
+                o_core2mem_we   = 1'b1;
+                o_core2mem_mask = 4'b0011;
+                o_core2mem_data = {2{i_data_core2mem[15:0]}};
             end
             SW: begin
-                o_data_core2mem_we   = 1'b1;
-                o_data_core2mem_mask = 4'b1111;
+                o_core2mem_we   = 1'b1;
+                o_core2mem_mask = 4'b1111;
             end
         endcase
     end
 
     always_comb begin
-        o_data_mem2core_data = i_data_mem2core;
+        o_mem2core_data = i_data_mem2core;
 
         case (i_mem_op)
-            LB:  o_data_mem2core_data = {{24{i_data_mem2core[7]}},  i_data_mem2core[7:0]};
-            LH:  o_data_mem2core_data = {{16{i_data_mem2core[15]}}, i_data_mem2core[15:0]};
-            LBU: o_data_mem2core_data = {24'b0, i_data_mem2core[7:0]};
-            LHU: o_data_mem2core_data = {16'b0, i_data_mem2core[15:0]};
-            LW:  o_data_mem2core_data = i_data_mem2core;
+            LB:  o_mem2core_data = {{24{i_data_mem2core[7]}},  i_data_mem2core[7:0]};
+            LH:  o_mem2core_data = {{16{i_data_mem2core[15]}}, i_data_mem2core[15:0]};
+            LBU: o_mem2core_data = {24'b0, i_data_mem2core[7:0]};
+            LHU: o_mem2core_data = {16'b0, i_data_mem2core[15:0]};
+            LW:  o_mem2core_data = i_data_mem2core;
         endcase
     end
 
--- problems/2025/05_cpu/src/core/core_pc.sv
+++ problems/2025/05_cpu/src/core/core_pc.sv
@@ -5,6 +5,7 @@ module core_pc (
     input  logic        i_branch,
     input  logic [31:0] i_branch_addr,
 
+    output logic [29:0] o_instr_addr,
     output logic [31:0] o_pc,
     output logic [31:0] o_pc_inc
 );
@@ -20,7 +21,8 @@ always_ff @(posedge clk or negedge rst_n)
     else
         pc_ff <= pc_next;
 
-assign o_pc     = {pc_ff, 2'b0};
-assign o_pc_inc = {pc_ff + 1'b1, 2'b0};
+assign o_instr_addr = pc_ff;
+assign o_pc         = {pc_ff, 2'b0};
+assign o_pc_inc     = {pc_ff + 1'b1, 2'b0};
 
 endmodule
