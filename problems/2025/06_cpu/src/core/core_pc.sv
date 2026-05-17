132a72a2af55944c6763a7a724c3adc01ea3aa43
Oleg Berezin <olegberezin2002@gmail.com>
2026-05-17 14:16:13 +0300

    Fix PC logic

--- problems/2025/06_cpu/src/core/core_pc.sv
+++ problems/2025/06_cpu/src/core/core_pc.sv
@@ -21,7 +21,7 @@ always_ff @(posedge clk or negedge rst_n)
     else
         pc_ff <= pc_next;
 
-assign o_instr_addr = !rst_n ? pc_next : (32'h1000) >> 2;
+assign o_instr_addr = rst_n ? pc_next : (32'h1000) >> 2;
 assign o_pc         = {pc_ff, 2'b0};
 assign o_pc_inc     = {pc_ff + 1'b1, 2'b0};
 
