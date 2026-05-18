796cdf445b0912f00bbba82de69103a77272f810
Oleg Berezin <olegberezin2002@gmail.com>
2026-05-18 22:28:14 +0300

    [08_cpu_v2] Fix indents

--- problems/2025/08_cpu/src/core/core_lsu.sv
+++ problems/2025/08_cpu/src/core/core_lsu.sv
@@ -15,41 +15,41 @@ module core_lsu
     output logic [31:0] o_mem2core_data
 );
 
-    assign o_core2mem_addr = 30'(({i_addr, 2'b0} + i_offset) >> 2);
+assign o_core2mem_addr = 30'(({i_addr, 2'b0} + i_offset) >> 2);
 
-    always_comb begin
-        o_core2mem_we   = 1'b0;
-        o_core2mem_mask = 4'b0000;
-        o_core2mem_data = i_data_core2mem;
+always_comb begin
+    o_core2mem_we   = 1'b0;
+    o_core2mem_mask = 4'b0000;
+    o_core2mem_data = i_data_core2mem;
 
-        case (i_mem_op)
-            SB: begin
-                o_core2mem_we   = 1'b1;
-                o_core2mem_mask = 4'b0001;
-                o_core2mem_data = {4{i_data_core2mem[7:0]}};
-            end
-            SH: begin
-                o_core2mem_we   = 1'b1;
-                o_core2mem_mask = 4'b0011;
-                o_core2mem_data = {2{i_data_core2mem[15:0]}};
-            end
-            SW: begin
-                o_core2mem_we   = 1'b1;
-                o_core2mem_mask = 4'b1111;
-            end
-        endcase
-    end
+    case (i_mem_op)
+        SB: begin
+            o_core2mem_we   = 1'b1;
+            o_core2mem_mask = 4'b0001;
+            o_core2mem_data = {4{i_data_core2mem[7:0]}};
+        end
+        SH: begin
+            o_core2mem_we   = 1'b1;
+            o_core2mem_mask = 4'b0011;
+            o_core2mem_data = {2{i_data_core2mem[15:0]}};
+        end
+        SW: begin
+            o_core2mem_we   = 1'b1;
+            o_core2mem_mask = 4'b1111;
+        end
+    endcase
+end
 
-    always_comb begin
-        o_mem2core_data = i_data_mem2core;
+always_comb begin
+    o_mem2core_data = i_data_mem2core;
 
-        case (i_mem_op)
-            LB:  o_mem2core_data = {{24{i_data_mem2core[7]}},  i_data_mem2core[7:0]};
-            LH:  o_mem2core_data = {{16{i_data_mem2core[15]}}, i_data_mem2core[15:0]};
-            LBU: o_mem2core_data = {24'b0, i_data_mem2core[7:0]};
-            LHU: o_mem2core_data = {16'b0, i_data_mem2core[15:0]};
-            LW:  o_mem2core_data = i_data_mem2core;
-        endcase
-    end
+    case (i_mem_op)
+        LB:  o_mem2core_data = {{24{i_data_mem2core[7]}},  i_data_mem2core[7:0]};
+        LH:  o_mem2core_data = {{16{i_data_mem2core[15]}}, i_data_mem2core[15:0]};
+        LBU: o_mem2core_data = {24'b0, i_data_mem2core[7:0]};
+        LHU: o_mem2core_data = {16'b0, i_data_mem2core[15:0]};
+        LW:  o_mem2core_data = i_data_mem2core;
+    endcase
+end
 
 endmodule
--- problems/2025/08_cpu/src/mem_xbar.v
+++ problems/2025/08_cpu/src/mem_xbar.v
@@ -25,35 +25,35 @@ module mem_xbar #(
     input  wire [31:0] i_mmio_data
 );
 
-    wire is_dmem_next;
-    wire is_mmio_next;
+wire is_dmem_next;
+wire is_mmio_next;
 
-    reg  is_dmem_ff;
-    reg  is_mmio_ff;
+reg  is_dmem_ff;
+reg  is_mmio_ff;
 
-    assign is_dmem_next = (i_addr >= DATA_START) && (i_addr <= DATA_LIMIT);
-    assign is_mmio_next = (i_addr >= MMIO_START) && (i_addr <= MMIO_LIMIT);
+assign is_dmem_next = (i_addr >= DATA_START) && (i_addr <= DATA_LIMIT);
+assign is_mmio_next = (i_addr >= MMIO_START) && (i_addr <= MMIO_LIMIT);
 
-    always @(posedge clk)
-        is_dmem_ff <= is_dmem_next;
+always @(posedge clk)
+    is_dmem_ff <= is_dmem_next;
 
-    always@(posedge clk)
-        is_mmio_ff <= is_mmio_next;
+always@(posedge clk)
+    is_mmio_ff <= is_mmio_next;
 
-    assign o_dmem_addr = i_addr;
-    assign o_mmio_addr = i_addr;
+assign o_dmem_addr = i_addr;
+assign o_mmio_addr = i_addr;
 
-    assign o_dmem_data = i_data;
-    assign o_mmio_data = i_data;
+assign o_dmem_data = i_data;
+assign o_mmio_data = i_data;
 
-    assign o_dmem_mask = i_mask;
-    assign o_mmio_mask = i_mask;
+assign o_dmem_mask = i_mask;
+assign o_mmio_mask = i_mask;
 
-    assign o_dmem_wren = i_wren & is_dmem_next;
-    assign o_mmio_wren = i_wren & is_mmio_next;
+assign o_dmem_wren = i_wren & is_dmem_next;
+assign o_mmio_wren = i_wren & is_mmio_next;
 
-    assign o_data = is_dmem_ff ? i_dmem_data
-                               : is_mmio_ff ? i_mmio_data
-                                            : 32'b0;
+assign o_data = is_dmem_ff ? i_dmem_data
+                           : is_mmio_ff ? i_mmio_data
+                                        : 32'b0;
 
 endmodule
