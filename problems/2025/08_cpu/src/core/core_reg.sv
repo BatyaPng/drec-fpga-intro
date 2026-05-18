c56424d892f1b596a19d5b267f604e82ffc79d62
Oleg Berezin <olegberezin2002@gmail.com>
2026-05-18 13:14:33 +0300

    [08_cpu] Add SVA for core_reg

--- problems/2025/08_cpu/src/core/core_reg.sv
+++ problems/2025/08_cpu/src/core/core_reg.sv
@@ -32,4 +32,24 @@ assign bypass_2 = (i_rs2 == i_rd) && reg_en;
 assign o_src1 = bypass_1 ? i_dst : reg_ff[i_rs1];
 assign o_src2 = bypass_2 ? i_dst : reg_ff[i_rs2];
 
+`ifndef FPGA
+    a_no_x_rs1: assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(i_rs1))
+        else $fatal(1, "i_rs1 is X or Z!");
+
+    a_no_x_rs2: assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(i_rs2))
+        else $fatal(1, "i_rs2 is X or Z!");
+
+    a_no_x_rd: assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(i_rd))
+        else $fatal(1, "i_rd is X or Z!");
+
+    a_no_x_dst: assert property (@(posedge clk) disable iff (!rst_n) (i_rd != 0) |-> !$isunknown(i_dst))
+        else $fatal(1, "i_dst is X or Z during an active write!");
+
+    a_no_x_src1: assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(o_src1))
+        else $fatal(1, "o_src1 is X or Z!");
+
+    a_no_x_src2: assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(o_src2))
+        else $fatal(1, "o_src2 is X or Z!");
+`endif
+
 endmodule
