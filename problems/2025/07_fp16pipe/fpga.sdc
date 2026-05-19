d82194d78387483b2571ed0824e00c2e51dfe6fd
Oleg Berezin <olegberezin2002@gmail.com>
2026-05-20 00:28:07 +0300

    [07_fp16pipe] Remove rst

--- problems/2025/07_fp16pipe/fpga.sdc
+++ problems/2025/07_fp16pipe/fpga.sdc
@@ -1,5 +1,3 @@
 create_clock -period "50.0 MHz" [get_ports CLK]
 
 derive_clock_uncertainty
-
-set_false_path -from [get_ports RSTN] -to [all_clocks]
--- problems/2025/07_fp16pipe/fpga_top.v
+++ problems/2025/07_fp16pipe/fpga_top.v
@@ -1,18 +1,10 @@
 module fpga_top(
     input  wire CLK,
-    input  wire RSTN,
 
     input  wire [15:0] i_a,
     input  wire [15:0] i_b,
     output reg  [15:0] o_res
 );
-
-reg rst_n, RSTN_d;
-
-always @(posedge CLK) begin
-    rst_n  <= RSTN_d;
-    RSTN_d <= RSTN;
-end
 
 reg  [15:0] a, b;
 wire [15:0] res;
@@ -24,6 +16,8 @@ always @(posedge CLK) begin
 end
 
 fp16add u_fp16add (
+   .clk      (CLK),
+
    .i_a      (a),
    .i_b      (b),
    .o_res    (res)
