2de42e5f4d62d37d8a583cbb202c9810fcd95d7a
Oleg Berezin <olegberezin2002@gmail.com>
2026-05-20 14:24:31 +0300

    [09_cpu] Make core_mux2 parameterized

--- problems/2025/09_cpu/src/core/core_mux2.sv
+++ problems/2025/09_cpu/src/core/core_mux2.sv
@@ -1,9 +1,11 @@
-module core_mux2 (
+module core_mux2 # (
+    parameter WIDTH = 32
+) (
     input logic  i_sel,
 
-    input logic [1:0][31:0] i_data,
+    input logic [1:0][WIDTH-1:0] i_data,
 
-    output logic [31:0] o_data
+    output logic [WIDTH-1:0] o_data
 );
 
 assign o_data = i_data[i_sel];
